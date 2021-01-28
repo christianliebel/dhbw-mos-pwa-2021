85163d579307e7779ffb1c7be5ff5bf3d24d2a90
Christian Liebel <christian.liebel@thinktecture.com>
2021-01-28 11:07:46 +0100

    Lab #9 - UI

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
+import { TodoService } from './../todo.service';
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  async getAll() {
+    this.todos = await this.todoService.getAll();
+  }
+
+  async add(title: string) {
+    await this.todoService.add(title);
+    this.getAll();
+  }
 }
